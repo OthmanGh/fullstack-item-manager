5d9834d764b46be73c869cad32fae6f0e142f283
Osman <othmanghandour51@gmail.com>
2024-12-17 23:59:32 +0200

    backend: initialize Spring Boot project setup

---
+++ backend/src/main/java/dev/othman/item/ItemManagerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.othman.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemManagerApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/othman/item/ItemManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.othman.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
