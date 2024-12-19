9a7f180e384567ee5ea6aa4c837a70ba22d1ff3d
Osman <othmanghandour51@gmail.com>
2024-12-19 20:39:26 +0200

    frontend: add middleware to handle public and protected routes

---
+++ frontend/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const validToken = process.env.NEXT_PUBLIC_AUTH_HASH
+  const authToken = request.cookies.get('authToken')?.value
+
+  const routes = {
+    protected: ['/'],
+    public: ['/login'],
+  }
+
+  const isProtectedRoute = routes.protected.includes(pathname)
+  const isPublicRoute = routes.public.includes(pathname)
+  const isAuthenticated = authToken === validToken
+
+  if (isProtectedRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  if (isPublicRoute && isAuthenticated) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/:path*', '/login'],
+}
