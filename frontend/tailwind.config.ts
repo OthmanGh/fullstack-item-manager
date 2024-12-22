5676eeb9fdbfce37c3963aad84740507f1d03c52
Osman <othmanghandour51@gmail.com>
2024-12-22 14:04:22 +0200

    frontend: configure tailwind css

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'light-gray': '#FAFAFA',
+        primary: '#633CFF',
+        'primary-hover': '#BEADFF',
+        'primary-light': '#EFEBFF',
+        'dark-grey': '#333333',
+        grey: '#737373',
+        borders: '#D9D9D9',
+        custom_red: '#EE3939',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
