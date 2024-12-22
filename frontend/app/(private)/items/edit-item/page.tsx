bff9fdd173ec51064b2cf9452fa486886f45fd35
Osman <othmanghandour51@gmail.com>
2024-12-22 13:53:16 +0200

    create edit item page

---
+++ frontend/app/(private)/items/edit-item/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import BackButton from '@/components/ui/BackButton'
+import CustomForm from '@/components/ui/CustomForm'
+import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+function EditItem() {
+  const searchParams = useSearchParams()
+  const itemParam = searchParams.get('item')
+  const [item, setItem] = useState(null)
+
+  useEffect(() => {
+    if (itemParam) {
+      try {
+        setItem(JSON.parse(itemParam))
+      } catch (e) {
+        console.error('Failed to parse item data:', e)
+      }
+    }
+  }, [itemParam])
+
+  if (!item) {
+    return (
+      <div className='text-gray-500 text-center my-10'>
+        No item data available.
+      </div>
+    )
+  }
+
+  const { id } = item
+
+  return (
+    <div className='sm:mx-10 mx-2 mt-10'>
+      <BackButton />
+      <section className='bg-white max-w-[650px] mx-auto shadow-sm sm:p-10 p-4 rounded-md'>
+        <h1 className='text-2xl font-bold text-center mb-4'>Edit Item</h1>
+        <CustomForm
+          initialState={item}
+          endpoint={`/items/${id}`}
+          action='put'
+        />
+      </section>
+    </div>
+  )
+}
+
+export default EditItem
