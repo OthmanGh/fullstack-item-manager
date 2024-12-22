42f0a0ed9cf2c683eb8e65510310d235c98c9bc9
Osman <othmanghandour51@gmail.com>
2024-12-22 22:04:02 +0200

    backend: enable MongoDB auditing in ItemRepository

--- backend/src/main/java/dev/othman/item/repository/ItemRepository.java
+++ backend/src/main/java/dev/othman/item/repository/ItemRepository.java
@@ -1,9 +1,12 @@
 package dev.othman.item.repository;
 
 import dev.othman.item.model.Item;
+
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableMongoAuditing
 public interface ItemRepository extends MongoRepository<Item, String> {
 }
