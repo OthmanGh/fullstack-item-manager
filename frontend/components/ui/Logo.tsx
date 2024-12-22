6412e56c2155f26e2dd3032bead0a0dcf1df810e
Osman <othmanghandour51@gmail.com>
2024-12-22 13:31:39 +0200

    frontend: add logo component

---
+++ frontend/components/ui/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <h1 className='text-dark-grey text-lg sm:text-3xl font-bold lowercase'>
+      <span className='capitalize'>Restful</span>
+      <span className='text-primary capitalize'>Hive</span>
+    </h1>
+  )
+}
+
+export default Logo
